207d1d23cb162385d93f03b7577b5ddcc1e607be
Qwertya <mrfoxikqr@gmail.com>
2026-08-13 08:07:37 +0300

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,4 +1,1 @@
-# Переходишь к функции catch_all_messages и меняешь 0 на 4
-async def catch_all_messages(message: types.Message):
-    from utils import delayed_delete
-    asyncio.create_task(delayed_delete(message, 4))
+
